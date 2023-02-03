13e19d1e4ea5cb0a05cfa1a8f36ca19e5f4a9855
= <Netzarigilbertol@hexaware.com>
2023-02-02 19:04:52 -0600

    Se sube la mayoria del framework, falta pulirlo para fase final

--- src/test/java/Hexaware/TestComponents/BaseTest.java
+++ src/test/java/Hexaware/TestComponents/BaseTest.java
@@ -1,5 +1,88 @@
 package Hexaware.TestComponents;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Properties;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import Hexaware.pageobjects.LoginPage;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class BaseTest {
+	public WebDriver driver;
+	public static LoginPage loginPage;
+	public WebDriver initializeDriver() throws IOException {
+		Properties prop = new Properties();
+		FileInputStream fis = new FileInputStream(System.getProperty("user.dir") + "\\src\\main\\java\\Hexaware\\resources\\GlobalData.properties");
+		prop.load(fis);
+		
+		String browserName = System.getProperty("browser") != null ? System.getProperty("browser") : prop.getProperty("browser");
+		
+		if(browserName.equals("chrome")) {
+			ChromeOptions opt = new ChromeOptions();
+			
+			if(browserName.contains("headless"))
+				opt.addArguments("headless");
+			
+			WebDriverManager.chromedriver().setup();			
+			driver = new ChromeDriver(opt);
+		}else if(browserName.equals("edge")) {
+			EdgeOptions opt = new EdgeOptions();
+			
+			if(browserName.contains("headless"))
+				opt.addArguments("headless");
+			
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver(opt);
+		}else if(browserName.equals("firefox")) {
+			FirefoxOptions opt = new FirefoxOptions();
+			
+			if(browserName.contains("headless"))
+				opt.addArguments("headless");
+			
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver(opt);
+		}
+		
+		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.manage().window().maximize();
+		return driver;
+	}
 	
+	public String getScreenshot(String testCaseName, WebDriver driver) throws IOException {
+		TakesScreenshot ts = (TakesScreenshot) driver;
+		String filePath = System.getProperty("user.dir") + "//reports//" + testCaseName + ".png";
+		File source = ts.getScreenshotAs(OutputType.FILE);
+		File fs = new File(filePath);
+		FileUtils.copyFile(source, fs);
+		return filePath;
+	}
+	
+	@BeforeMethod(alwaysRun=true)
+	public LoginPage launchingApplication() throws IOException {
+		driver = initializeDriver();
+		loginPage = new LoginPage(driver);
+		loginPage.goToLoginPage();
+		return loginPage;
+	}
+	
+	@AfterMethod(alwaysRun=true)
+	public void tearDown() {
+		driver.quit();
+	}
 }
---
+++ src/test/java/Hexaware/data/DataReader.java
@@ -0,0 +1,38 @@
+package Hexaware.data;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class DataReader {
+	DataFormatter formatter = new DataFormatter();
+	public Object[][] DataRetrieveFromExcel(String path) throws IOException {		
+		FileInputStream fis = new FileInputStream(path);
+		@SuppressWarnings("resource")
+		XSSFWorkbook wb = new XSSFWorkbook(fis);
+		XSSFSheet sheet = wb.getSheetAt(0);
+		
+		int rowCount = sheet.getPhysicalNumberOfRows();
+		XSSFRow row = sheet.getRow(0);
+		int columnCount = row.getLastCellNum();
+		
+		Object[][] data = new Object[rowCount-1][columnCount];
+		
+		for(int i=0;i<rowCount-1;i++) {
+//			System.out.println(rowCount-1);
+			row = sheet.getRow(i+1);
+			for(int j=0;j<columnCount;j++) {
+				XSSFCell cell = row.getCell(j);				
+				data[i][j] = formatter.formatCellValue(cell);
+//				System.out.println(data[i][j]);
+			}
+		}
+		
+		return data;
+	}
+}
